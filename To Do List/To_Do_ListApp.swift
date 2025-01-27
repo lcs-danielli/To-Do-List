1e1b1b0f1f89b5f02d0df70d18bcd455b4338ce7
Daniel Li <danli@lcs.on.ca>
2025-01-27 09:36:06 -0500

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by 李泽宇 on 2025-01-27.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
