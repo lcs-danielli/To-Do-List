d7c50ae7a057bafd155956c246d855910bd29444
Daniel Li <danli@lcs.on.ca>
2025-01-30 20:05:56 -0500

    put ItemView into its own file

--- To Do List/Views/ItemView.swift
+++ To Do List/Views/ItemView.swift
@@ -5,3 +5,28 @@
 //  Created by 李泽宇 on 2025-01-27.
 //
 
+import SwiftUI
+
+struct ItemView: View {
+    
+    let title: String
+    let done: Bool
+    
+    var body: some View {
+        Label(
+            title: {
+                Text(title)
+            }, icon: {
+                Image(systemName: done == true ? "checkmark.circle" : "circle ")
+                
+            }
+        )
+    }
+}
+
+#Preview {
+    List{
+        ItemView(title: "Have a nap", done: false)
+        ItemView(title: "Eat luunch", done: true)
+    }
+}
